d46cacd07be3d0690fe6093e83dd483286e43ddb
tanitta <ttata.trit@gmail.com>
2014-05-20 21:35:15 +0900

    LocalMatrix作った

--- src/Core.hpp
+++ src/Core.hpp
@@ -2,18 +2,23 @@
 
 #include "ofMain.h"
 #include "data/MatrixManager.hpp"
+#include "data/Cell.hpp"
 #include "graphics/Drawer.hpp"
 #include "test/VirtualClass.hpp"
-// #include "boost/multi_array.hpp"
+#include "boost/multi_array.hpp"
 
 namespace unitro{
 	class Core
 	{
 	public:
-		Core(){};
-		~Core(){};
+		ofVec3f matrixSize;
 		
+		Core():matrixSize(ofVec3f(10,10,10)){};
+		~Core(){};
 		void setup(){
+			currentLocalMatrix.resize(boost::extents[matrixSize.x][matrixSize.y][matrixSize.z]);
+			nextLocalMatrix.resize(boost::extents[matrixSize.x][matrixSize.y][matrixSize.z]);
+			
 			ofSetFrameRate(60);
 			drawer.setup();
 		};
@@ -28,5 +33,8 @@ namespace unitro{
 		unitro::graphics::Drawer drawer;
 		
 		// unitro::test::VirtualClass* hoge[10];
+		boost::multi_array<data::Cell, 3> currentLocalMatrix;
+		boost::multi_array<data::Cell, 3> nextLocalMatrix;
+		
 	};
 }
