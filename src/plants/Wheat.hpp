e7afd699facab95b3711d8b19664895762a9fb73
tanitta <ttata.trit@gmail.com>
2014-06-20 02:06:16 +0900

    Revert "Add ofx3DModelLoader(static) in Wheat Class"
    This reverts commit 33c60dccc8308befa618c044b365abdf1a2d8f9c.

--- src/plants/Wheat.hpp
+++ src/plants/Wheat.hpp
@@ -1,16 +1,11 @@
 #pragma once
 #include "BasePlant.hpp"
-#include "ofx3DModelLoader.h"
 namespace unitro{
 namespace plants{
 	class Wheat : public unitro::plants::BasePlant
 		{
 		public:
-			static ofx3DModelLoader modelObj;
-			
-			Wheat(){
-				modelObj.loadModel("modelSample.3ds", 1 );
-			};
+			Wheat(){};
 			~Wheat(){};
 			
 			void setup(){};
