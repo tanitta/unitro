2751b39c0be31f1e8cf77b2458eeb6803557bc1e
tanitta <ttata.trit@gmail.com>
2014-06-22 00:47:41 +0900

    Avoid masic number of framerate

--- src/Core.hpp
+++ src/Core.hpp
@@ -6,36 +6,36 @@
 #include "boost/multi_array.hpp"
 #include "solver.hpp"
 #include "Env.h"
+#include "data/Matrix.hpp"
 namespace unitro{
 	class Core
 	{
 	public:
-		// ofVec3f unitro::env::General::matrixSize;
+		// ofVec3f Punitro::env::General::matrixSize;
 		boost::multi_array<data::Cell, 3> currentLocalMatrix;
 		boost::multi_array<data::Cell, 3> nextLocalMatrix;
 		unitro::graphics::Drawer drawer;
 		unitro::Solver solver;
 		int counter;
-		Core():counter(0){};
+		Core():counter(0){
+			unitro::data::currentLocalMatrix.resize(boost::extents[unitro::env::General::matrixSize.x][unitro::env::General::matrixSize.y][unitro::env::General::matrixSize.z]);
+			unitro::data::nextLocalMatrix.resize(boost::extents[unitro::env::General::matrixSize.x][unitro::env::General::matrixSize.y][unitro::env::General::matrixSize.z]);
+		};
 		~Core(){};
 		
 		void setup(){
-			currentLocalMatrix.resize(boost::extents[unitro::env::General::matrixSize.x][unitro::env::General::matrixSize.y][unitro::env::General::matrixSize.z]);
-			nextLocalMatrix.resize(boost::extents[unitro::env::General::matrixSize.x][unitro::env::General::matrixSize.y][unitro::env::General::matrixSize.z]);
-			
-			ofSetFrameRate(60);
-			drawer.setup(nextLocalMatrix);
-			solver.setup(currentLocalMatrix,nextLocalMatrix);
-			solver.startThread(true,false);
+			ofSetFrameRate(unitro::env::General::frameRate);
+			// drawer.setup();
+			// solver.setup();
+			// solver.startThread(true,false);
 		};
 		void update(){
-			// solver.update(currentLocalMatrix,nextLocalMatrix);
 			if (counter >= 10*60-1)
 			{
 				if (!solver.isThreadRunning()){
 					cout<<"thread is end"<<endl;
-					solver.stopThread();
-					solver.startThread(true,false);
+					// solver.stopThread();
+					// solver.startThread(true,false);
 					counter = 0;
 				}
 			}else{
@@ -45,13 +45,12 @@ namespace unitro{
 		void draw(){
 			ofPushMatrix();
 				ofScale(4,4,4);
-				drawer.draw(nextLocalMatrix);
-				//solver.draw(currentLocalMatrix,nextLocalMatrix);
+				// drawer.draw(nextLocalMatrix);
 			ofPopMatrix();
 		};
 		
 		void exit(){
-			solver.stopThread();
+			// solver.stopThread();
 		};
 	};
 }
