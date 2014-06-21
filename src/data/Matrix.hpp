540c8744d2b231b5d3fc39ec1d0ca06fb45771ee
tanitta <ttata.trit@gmail.com>
2014-06-22 00:35:27 +0900

    Add matrix instances to Matrix.hpp(unitro::data)

--- src/data/Matrix.hpp
+++ src/data/Matrix.hpp
@@ -1,1 +1,8 @@
-#include "boost/"
+#include "boost/multi_array.hpp"
+##include "../Env.h"
+namespace unitro{
+namespace data{
+	boost::multi_array<data::Cell, 3> currentLocalMatrix;
+	boost::multi_array<data::Cell, 3> nextLocalMatrix;
+}
+}
