5313bab7426716736e96fd4f4c83a14f653678aa
tanitta <ttata.trit@gmail.com>
2014-08-19 10:48:26 +0900

    Enable to work multiple keyboard input

---
+++ src/interface/Keyboard.cpp
@@ -0,0 +1,4 @@
+#include "Keyboard.hpp"
+vector<bool> unitro::interface::Keyboard::isKey(128,false);
+vector<bool> unitro::interface::Keyboard::isKeyDown(128,false);
+vector<bool> unitro::interface::Keyboard::isKeyUp(128,false);
