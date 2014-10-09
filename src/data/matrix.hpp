9ac01d8751d32f05be87813f5fdcc55b2dff812e
tanitta <ttata.trit@gmail.com>
2014-10-09 23:23:01 +0900

    Add operators to Matrix

--- src/data/matrix.hpp
+++ src/data/matrix.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "boost/multi_array.hpp"
+#include <boost/numeric/ublas/vector.hpp>
 #include "cell.hpp"
 // #include "../Env.h"
 namespace unitro{
@@ -9,8 +10,28 @@ namespace data{
 			std::vector< std::vector<std::vector<unitro::data::Cell>>> matrix_;
 		public:
 			Matrix(int x, int y, int z):
-		matrix_(x,std::vector<std::vector<Cell>>(y,std::vector<Cell>(z))){};
+				matrix_(x,std::vector<std::vector<Cell>>(y,std::vector<Cell>(z))){};
 			virtual ~Matrix(){};
+
+			boost::numeric::ublas::vector<int> GetSize(){
+				boost::numeric::ublas::vector<int> sizes(3);
+				sizes[0] = matrix_.size();
+				sizes[1] = matrix_[0].size();
+				sizes[2] = matrix_[0][0].size();
+				return sizes;
+			}
+
+			std::vector<std::vector<Cell>>& operator[](int i){
+				return matrix_[i];
+			}
+
+			Matrix& operator=(std::vector<std::vector<std::vector<Cell>>>& a){
+				return *this;
+			}
+
+			unitro::data::Cell& operator()(boost::numeric::ublas::vector<int> v){
+				return matrix_[v[0]][v[1]][v[2]];
+			}
 	};
 }
 }
