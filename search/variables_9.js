8106358f66a2e500162d9d38aae04ea01beca364
tanitta <ttata.trit@gmail.com>
2014-10-23 20:53:44 +0900

    updated docs

--- search/variables_9.js
+++ search/variables_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['plant_5f',['plant_',['../classunitro_1_1data_1_1Cell.html#ae8ed6fa6572c835bd688ca8cc059990d',1,'unitro::data::Cell']]],
+  ['player2_5f',['player2_',['../classunitro_1_1Client.html#a1cc8e6fd60cf74d62ff36865ffc892f4',1,'unitro::Client']]],
   ['player_5f',['player_',['../classunitro_1_1Client.html#a12257eb0712f8d299ed700eee8b453eb',1,'unitro::Client']]],
   ['player_5fref_5f',['player_ref_',['../classunitro_1_1LocalPlayerController.html#ab1a78da41bc18935b729e8ce3f4aaf04',1,'unitro::LocalPlayerController::player_ref_()'],['../classunitro_1_1LocalPlayerView.html#a984e9746c39bbcfd0c94ee031bf34a70',1,'unitro::LocalPlayerView::player_ref_()'],['../classunitro_1_1LocalWorldController.html#a054436927398a0c2415997e252c30f6f',1,'unitro::LocalWorldController::player_ref_()'],['../classunitro_1_1LocalWorldView.html#a6951f1947afdc201d31c18fc5e7d13cf',1,'unitro::LocalWorldView::player_ref_()']]],
   ['pos_5f',['pos_',['../classunitro_1_1entity_1_1KineticModel.html#a1f4f779343e1595b4233533f3ee6d7e7',1,'unitro::entity::KineticModel']]]
