30e168cf60611cd758aa5ed16b64e3042d049450
tanitta <ttata.trit@gmail.com>
2014-12-03 02:35:46 +0900

    :q

--- src/solver.hpp
+++ src/solver.hpp
@@ -7,18 +7,17 @@ namespace unitro {
 	class Solver {
 		private:
 			unitro::data::Matrix& matrix_ref_;
-
+			int counter_;
+			std::thread thread_;
 		public:
-			Solver(unitro::data::Matrix& matrix_ref):matrix_ref_(matrix_ref)
-			{
-			};
+			Solver(unitro::data::Matrix& matrix_ref):matrix_ref_(matrix_ref),counter_(0){};
 			virtual ~Solver(){};
 
 			void UpdateSoil(){};
 			void UpdateWater(){};
 			void UpdatePlant(){};
 
-			void Update(){
+			void UpdateThread(){
 
 				std::thread thread_soil([=]{this->UpdateSoil();});
 				std::thread thread_water([=]{this->UpdateWater();});
@@ -27,5 +26,17 @@ namespace unitro {
 				thread_water.join();
 				thread_plant.join();
 			};
+			void Update(){
+				if(counter_ == 0){thread_ = std::thread([=]{this->UpdateThread();});}
+
+				counter_ ++;
+
+				if(counter_ >=30){
+					thread_.join();
+					counter_ = 0;
+				}
+			};
+			void StartThread(){};
+			void EndThread(){};
 	};
 } // namespace unitro
