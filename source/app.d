77f015b54f769384d0dcdbf6c7fa480eda2e9b74
tanitta <ttata.trit@gmail.com>
2015-11-21 08:53:43 +0900

    start to remaking in D lang

---
+++ source/app.d
@@ -0,0 +1,15 @@
+import std.stdio;
+import std.math;
+import armos.app;
+
+class TestApp : armos.app.BaseApp{
+	void setup(){}
+	
+	void update(){}
+	
+	void draw(){}
+}
+
+void main(){
+	armos.app.run(new TestApp);
+}
