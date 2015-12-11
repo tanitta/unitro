13f24a5aae838785490056fa86d71e0b18347a24
tanitta <ttata.trit@gmail.com>
2015-12-11 23:28:30 +0900

    add LocalGrid

--- source/app.d
+++ source/app.d
@@ -52,11 +52,11 @@ class Cell{
 class Grid{
 	Cell[][][] data;
 	private ar.Vector3i origin_;
-	ar.Vector3i origin()const{return origin_;}
 	
-	ar.Vector3i size(){return ar.Vector3i(cast(int)data.length, cast(int)data[0].length, cast(int)data[0][0].length);}
-	ar.Vector3i lowerRange(){return -origin_;}
-	ar.Vector3i upperRange(){return size+lowerRange-ar.Vector3i(1, 1, 1);}
+	ar.Vector3i origin()const{return origin_;}
+	ar.Vector3i size()const{return ar.Vector3i(cast(int)data.length, cast(int)data[0].length, cast(int)data[0][0].length);}
+	ar.Vector3i lowerRange()const{return -origin_;}
+	ar.Vector3i upperRange()const{return size+lowerRange-ar.Vector3i(1, 1, 1);}
 	
 	this(in ar.Vector3i size, in ar.Vector3i origin_coordination = ar.Vector3i(0, 0, 0)){
 		origin_ = origin_coordination;
@@ -71,40 +71,6 @@ class Grid{
 		}
 	}
 	
-	// void each_alias(alias f)(){
-	// 	foreach (int i, ref a; data) {
-	// 		foreach (int j, ref b; a) {
-	// 			foreach (int k, ref cell; b) {
-	// 				f(ar.Vector3i(i, j, k)-origin, data[i][j][k]);
-	// 			}
-	// 		}
-	// 	}
-	// }
-	//
-	// void each(void delegate(ar.Vector3i position, Cell cell) f){
-	// 	// for (int i = 0; i < size[0]; i++) {
-	// 	// 	for (int j = 0; j < size[1]; j++) {
-	// 	// 		for (int k = 0; k < size[2]; k++) {
-	// 	// 			f(ar.Vector3i(i, j, k)-origin, data[i][j][k]);
-	// 	// 		}
-	// 	// 	}
-	// 	// }
-	// 	foreach (int i, ref a; data) {
-	// 		foreach (int j, ref b; a) {
-	// 			foreach (int k, ref cell; b) {
-	// 				f(ar.Vector3i(i, j, k)-origin, data[i][j][k]);
-	// 			}
-	// 		}
-	// 	}
-	// }
-	//
-	// void each(void delegate(ar.Vector3i) f){
-	// 	each((ar.Vector3i position, Cell cell){f(position);});
-	// }
-	//
-	// void each(void delegate(Cell) f){
-	// 	each((ar.Vector3i position, Cell c){f(c);});
-	// }
 	unittest{
 		auto grid = new Grid(ar.Vector3i(10, 10, 10));
 		grid.each!((ar.Vector3i vec,  Cell cell ){ cell.soil = 2.0; } );
@@ -112,22 +78,6 @@ class Grid{
 		assert(grid.cell(7, 7, 7).soil == 2.0);
 	}
 	
-	// void eachNBHD(
-	// 	void delegate(NBHD nbhd) f,
-	// 	in ar.Vector3i lower_range,
-	// 	in ar.Vector3i upper_range 
-	// ){
-	// 	each((ar.Vector3i position){
-	// 		f(this.nbhd(position, lower_range, upper_range));
-	// 	});
-	// }
-	//
-	// void eachNBHD(
-	// 	void delegate(NBHD nbhd) f,
-	// 	in ar.Vector3i range,
-	// ){
-	// 	eachNBHD(f, -range, range);
-	// }
 	
 	NBHD nbhd(
 		in ar.Vector3i target_cell_position,
@@ -187,14 +137,6 @@ struct NBHD{
 	private ar.Vector3i upperRange_;
 	private ar.Vector3i targetCellPosition;
 	
-	ar.Vector3i lowerRange()const{
-		return lowerRange_;
-	}
-	
-	ar.Vector3i upperRange()const{
-		return upperRange_;
-	}
-	
 	this(
 		Grid source_grid,
 		in ar.Vector3i target_cell_position,
@@ -219,19 +161,27 @@ struct NBHD{
 		}
 	}
 	
-	ar.Vector3i size(){
+	ar.Vector3i size()const{
 		return -lowerRange + upperRange + ar.Vector3i(1, 1, 1);
 	}
 	
-	ar.Vector3i origin(){
+	ar.Vector3i origin()const{
 		return -lowerRange;
 	}
 	
-	Cell cell(ar.Vector3i local_pos){
+	ar.Vector3i lowerRange()const{
+		return lowerRange_;
+	}
+	
+	ar.Vector3i upperRange()const{
+		return upperRange_;
+	}
+	
+	Cell cell(in ar.Vector3i local_pos){
 		return grid.cell(local_pos+targetCellPosition);
 	}
 	
-	Cell cell(int x, int y, int z){
+	Cell cell(in int x, in int y, in int z){
 		return cell(ar.Vector3i(x, y, z));
 	}
 	
@@ -273,16 +223,19 @@ unittest{
 	auto grid = new Grid(ar.Vector3i(10, 10, 10), ar.Vector3i(2, 2, 2));
 	grid.cell(1, 1, 1).soil = 2;
 	
-	auto nbhd = new NBHD(
+	auto nbhd = NBHD(
 		grid,
 		ar.Vector3i(1, 1, 1), 
 		ar.Vector3i(-1, -1, -1), 
 		ar.Vector3i(1, 1, 1),
 	);
 	assert(nbhd.cell(0, 0, 0).soil == 2);
+	
+	auto nbhd1 = nbhd.nbhd(ar.Vector3i(1, 1, 1), ar.Vector3i(1, 1, 1));
+	assert(nbhd1.cell(-1, -1, -1).soil == 2);
 }
 
-class World{
+class GlobalWorld{
 	private Config config;
 	Grid grid;
 	DynamicEntity[] entities;
@@ -293,10 +246,22 @@ class World{
 	}
 }
 
+class LocalWorld{
+	private Config config;
+	Grid grid;
+	DynamicEntity[] entities;
+	this(Config conf){
+		config = conf;
+		auto grid_size = config.renderingRange*2 +ar.Vector3i(1, 1, 1);
+		auto origin = config.renderingRange;
+		grid = new Grid(grid_size, origin); 
+	}
+}
+
 class Solver{
 	private Config config;
-	private World world;
-	this(Config confg, World world){
+	private GlobalWorld world;
+	this(Config confg, GlobalWorld world){
 		this.config = config;
 		this.world = world;
 	}
@@ -308,60 +273,72 @@ class Solver{
 
 class Renderer{
 	private Config config;
-	private World world;
+	private GlobalWorld world;
 	
 	ar.Camera camera;
 
-	this(Config config, World world){
+	this(Config config, GlobalWorld world){
 		this.config = config;
 		this.world = world;
 		camera = new ar.Camera;
 		camera.position = ar.Vector3f(0, 0, -40);
-		camera.target= ar.Vector3f(4, 4, 0);
+		camera.target= ar.Vector3f(2, 4, 0);
 	}
 	
 	int counter = 0;
 	
 	void drawWorld(){
 		counter++;
-		camera.start;
+		camera.begin;
 		ar.rotate(counter, ar.Vector3f(1, 1, 1));
 		auto cameraTargetNBHD = world.grid.nbhd(cast(ar.Vector3i)camera.target, config.renderingRange);
 		auto func = delegate void(ar.Vector3i position, Cell cell){
 			ar.pushMatrix;
 			ar.translate(cast(ar.Vector3f)position + camera.target);
-			// auto nbhd = cameraTargetNBHD.nbhd(position, ar.Vector3i(1, 1, 1));
 			cell.draw(
 				cameraTargetNBHD.nbhd(position, ar.Vector3i(1, 1, 1))
 			);
 			ar.popMatrix;
 		};
 		cameraTargetNBHD.each!(func);
-		// cameraTargetNBHD.each(func);
-		// cameraTargetNBHD.each(
-		// 	(ar.Vector3i position, Cell cell){
-		// 		ar.pushMatrix;
-		// 		ar.translate(cast(ar.Vector3f)position + camera.target);
-		// 		auto nbhd = cameraTargetNBHD.nbhd(position, ar.Vector3i(1, 1, 1));
-		// 		cell.draw(
-		// 			cameraTargetNBHD.nbhd(position, ar.Vector3i(1, 1, 1))
-		// 		);
-		// 		ar.popMatrix;
-		// 	}
-		// );
 		camera.end;
 	};
 }
 
+class Network{
+	GlobalWorld world;
+	this(GlobalWorld world){
+		this.world = world;
+	}
+	
+	
+	
+	NBHD nbhd(){
+		return NBHD();
+	};
+	
+	void updateCell(
+		in ar.Vector3i globalPosition,
+		in ar.Vector3i lowerRange,
+		in ar.Vector3i upperRange
+	){
+	
+	};
+	
+	// void touchCell(in ar.Vector3i localPosition, in ToolType toolType){}
+}
+
 class TestApp : ar.BaseApp{
 	private Config config;
-	private World world;
+	private GlobalWorld world;
+	private Network network;
 	private Solver solver;
 	private Renderer renderer;
 	
 	this(){
 		config = new Config();
-		world = new World(config);
+		world = new GlobalWorld(config);
+		network = new Network(world);
 		solver = new Solver(config, world);
 		renderer = new Renderer(config, world);
 		
@@ -373,7 +350,7 @@ class TestApp : ar.BaseApp{
 	}
 	
 	void setup(){
-		ar.targetFps = 30;
+		ar.targetFps = 60;
 	}
 	
 	void update(){
@@ -384,16 +361,6 @@ class TestApp : ar.BaseApp{
 		renderer.drawWorld;
 		( ar.fpsUseRate*100 ).writeln;
 	}
-	
-	void keyPressed(int key){}
-	
-	void keyReleased(int key){}
-	
-	void mouseMoved(ar.Vector2i position, int button){}
-	
-	void mousePressed(ar.Vector2i position, int button){}
-	
-	void mouseReleased(ar.Vector2i position, int button){}
 }
 
 void main(){ar.run(new TestApp);}
