db3553723bf7a980106c412dc1eed9199bf55ff7
tanitta <ttata.trit@gmail.com>
2016-01-14 00:36:09 +0900

    enable to draw each cell

--- source/app.d
+++ source/app.d
@@ -15,10 +15,19 @@ class Config {
 ++/
 struct GridRange{
 	public{
+		/++
+		++/
 		ar.Vector3i lowerRange;
+		
+		/++
+		++/
 		ar.Vector3i upperRange;
+		
+		/++
+		++/
 		this(ar.Vector3i lower, ar.Vector3i upper){
-
+			lowerRange = lower;
+			upperRange = upper;
 		};
 	}//public
 }
@@ -44,6 +53,8 @@ struct Grid{
 		ar.Vector3i upperRange()const{return -( _size - _origin - ar.Vector3i(1, 1, 1) );};
 		GridRange gridRange()const{return GridRange(lowerRange, upperRange);}
 
+		/++
+		++/
 		this(ar.Vector3i size)
 			in{
 				assert(size[0]>0);
@@ -56,6 +67,8 @@ struct Grid{
 			allocate();
 		}
 
+		/++
+		++/
 		this(ar.Vector3i size, ar.Vector3i origin)
 			in{
 				assert(size[0]>0);
@@ -73,20 +86,28 @@ struct Grid{
 			assert(grid.origin == ar.Vector3i(0, 0, 0));
 		}
 
+		/++
+		++/
 		Cell cell(in int x, in int y, in int z)const{
 			return cell(ar.Vector3i(x, y, z));
 		}
 
+		/++
+		++/
 		Cell cell(ar.Vector3i position)const{
 			auto localPosition = position + origin;
 			auto index = localPosition[0] + localPosition[1]*size[0] + localPosition[2]*size[0]*size[1];
 			return cast(const(Cell))data[index];
 		}
 
+		/++
+		++/
 		void cell(in Cell c, in int x, in int y, in int z){
 			cell(c, ar.Vector3i(x, y, z));
 		}
 
+		/++
+		++/
 		void cell(in Cell c, in ar.Vector3i position){
 			auto localPosition = position + origin;
 			auto index = localPosition[0] + localPosition[1]*size[0] + localPosition[2]*size[0]*size[1];
@@ -101,11 +122,15 @@ struct Grid{
 			assert(grid.cell(1, 2, 3).soil == 1.0);
 		}
 
+		/++
+		++/
 		Grid nbhd(ar.Vector3i target, ar.Vector3i lower, ar.Vector3i upper)const{
 			auto grid = Grid(-lower + upper + ar.Vector3i(1, 1, 1), -lower);
 			return grid;
 		}
 
+		/++
+		++/
 		void setNbhd(in Grid sourceGrid, in ar.Vector3i targetPosition){
 			for (int i = sourceGrid.lowerRange[0]; i <= sourceGrid.upperRange[0]; i++) {
 				for (int j = sourceGrid.lowerRange[1]; j <= sourceGrid.upperRange[1]; j++) {
@@ -118,6 +143,8 @@ struct Grid{
 	}//public
 
 	private{
+		/++
+		++/
 		void allocate(){
 			auto length = size[0]*size[1]*size[2];
 			for(int i = 0; i < length; i++){
@@ -195,11 +222,14 @@ void each(alias f, G)(G grid, ar.Vector3i lowerRange, ar.Vector3i upperRange){
 ++/
 struct LocalWorld {
 	public{
+		/++
+		++/
 		Grid nbhd(ar.Vector3i target, ar.Vector3i lower, ar.Vector3i upper){return Grid();}
 
 		void nbhd(in Grid grid, in ar.Vector3i target){};
+		
 		this(in GlobalWorld globalWorld, in ar.Vector3i targetPosition, in GridRange range){
-			grid = globalWorld.nbhd(targetPosition, range.lowerRange, range.lowerRange);
+			grid = globalWorld.nbhd(targetPosition, range.lowerRange, range.upperRange);
 		}
 	}//public
 
@@ -212,26 +242,37 @@ struct LocalWorld {
 ++/
 class GlobalWorld{
 	public{
+		
+		/++
+		++/
 		this(ar.Vector3i size){
-			grid = Grid(size);
+			_grid = Grid(size);
 		};
 
+		/++
+		++/
 		Grid nbhd(in ar.Vector3i targetPosition, in ar.Vector3i lower, in ar.Vector3i upper)const{
+			lower.print;
+			upper.print;
 			auto grid = Grid(-lower + upper + ar.Vector3i(1, 1, 1), -lower);
 			return grid;
 		};
 
+		/++
+		++/
 		void setLocalWorld(in LocalWorld localWorld, in ar.Vector3i targetPosition){
-			grid.setNbhd(localWorld.grid, targetPosition);
+			_grid.setNbhd(localWorld.grid, targetPosition);
 		}
 
+		/++
+		++/
 		void getLocalWorld(ref LocalWorld localWorld, in ar.Vector3i targetPosition, in GridRange range)const{
 			localWorld = LocalWorld(this, targetPosition, range);
 		}
 	}//public
 
 	private{
-		Grid grid;
+		Grid _grid;
 	}//private
 }
 
@@ -245,31 +286,41 @@ struct PlayerEvent{
 ++/
 class Network{
 	public{
+		/++
+		++/
 		this(Config config){
-			this.config = config;
-			world = new GlobalWorld(config.worldSize);
+			_config = config;
+			_world = new GlobalWorld(config.worldSize);
 		}
 
+		/++
+		++/
 		void setLocalWorld(in LocalWorld localWorld, ar.Vector3i targetPosition){
-			world.setLocalWorld(localWorld, targetPosition);
+			_world.setLocalWorld(localWorld, targetPosition);
 			sync();
 		}
 
-		void getLocalWorld(ref LocalWorld localWorld, ar.Vector3i targetPosition, GridRange range){
-			world.getLocalWorld(localWorld, targetPosition, range);
+		/++
+		++/
+		void getLocalWorld(ref LocalWorld localWorld, in ar.Vector3i targetPosition, in GridRange range){
+			_world.getLocalWorld(localWorld, targetPosition, range);
 			sync();
 		}
 
+		/++
+		++/
 		void setPlayerEvent(PlayerEvent){
 			sync();
 		};
 
+		/++
+		++/
 		void sync(){}
 	}//public
 
 	private{
-		GlobalWorld world;
-		Config config;
+		GlobalWorld _world;
+		Config _config;
 	}//private
 }
 
@@ -284,10 +335,14 @@ class CellBox{
 
 class ResourceManager {
 	public{
+		/++
+		++/
 		void load(in string fileName){
 			
 		}
 		
+		/++
+		++/
 		ResourceEntity opIndex(in string name){
 			return data[name];
 		}
@@ -302,11 +357,20 @@ class ResourceManager {
 ++/
 class CellRenderer{
 	public{
-		ar.Mesh soilMesh = ar.boxPrimitive(ar.Vector3f(0, 0, 0), ar.Vector3f(1, 1, 1));
-		void draw(in Grid nbhd)const{
-			
+		/++
+		++/
+		void draw(in Grid nbhd){
+			nbhd.each!((ar.Vector3i position, Cell cell){
+				ar.pushMatrix;scope(exit)ar.popMatrix;
+				ar.translate(cast(ar.Vector3f)position);
+				soilMesh.drawWireFrame;
+			});
 		}
 	}//public
+	
+	private{
+		ar.Mesh soilMesh = ar.boxPrimitive(ar.Vector3f(0, 0, 0), ar.Vector3f(1, 1, 1));
+	}//private
 }
 
 
@@ -314,26 +378,38 @@ class CellRenderer{
 ++/
 class WorldRenderer {
 	public{
-		Config config;
-		Network network;
-		CellRenderer cellRenderer;
-		LocalWorld _localWorld;
-		ar.Camera camera;
+		/++
+		++/
 		this(Config config, Network network){
-			this.config = config;
-			cellRenderer = new CellRenderer;
-			camera = new ar.Camera;
-			camera.target = ar.Vector3f(5, 5, 5);
+			_config = config;
+			_network = network;
+			_cellRenderer = new CellRenderer;
+			_camera = new ar.Camera;
+			_camera.target = ar.Vector3f(5, 5, 5);
+			_camera.position = ar.Vector3f(5, 5, -20);
 		}
 
+		/++
+		++/
+		double angle = 0;
 		void draw(){
-			// network.getLocalWorld(_localWorld, cast(ar.Vector3i)camera.target, GridRange(ar.Vector3i(-3, -3, -3), ar.Vector3i(3, 3, 3)));
-			cellRenderer.draw(_localWorld.grid);
+			_network.getLocalWorld(_localWorld, cast(ar.Vector3i)_camera.target, GridRange(ar.Vector3i(-3, -3, -3), ar.Vector3i(3, 3, 3)));
+			angle += 0.1;
+			_camera.begin;
+				ar.pushMatrix;
+					ar.rotate(angle, 1, 1, 1);
+					_cellRenderer.draw(_localWorld.grid);
+				ar.popMatrix;
+			_camera.end;
 		};
 	}//public
 
 	private{
-		LocalWorld world;
+		Config _config;
+		Network _network;
+		CellRenderer _cellRenderer;
+		LocalWorld _localWorld;
+		ar.Camera _camera;
 	}//private
 }
 
@@ -341,17 +417,22 @@ class WorldRenderer {
 ++/
 class Solver {
 	public{
-		Config config;
-		Network network;
+		/++
+		++/
 		this(Config config, Network network){
-			this.config = config;
+			_config = config;
+			_network = network;
 		}
 
+		/++
+		++/
 		void solve(){};
 	}//public
 
 	private{
-		LocalWorld world;
+		Config _config;
+		Network _network;
+		LocalWorld _world;
 	}//private
 }
 
@@ -382,8 +463,6 @@ class TestApp : ar.BaseApp{
 
 	private{
 		Config config;
-		// GlobalWorld world;
-		// Solver solver;
 		Network network;
 		WorldRenderer renderer;
 		Solver solver;
